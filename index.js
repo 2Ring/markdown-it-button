fa87487811567d9f076ed1ffbe2f9f4cee624e42
Radovan Balog <radovan.balog@2ring.com>
2018-09-24 10:00:23 +0200

    Ignore carets

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mdRegexp = require('markdown-it-regexp');
 
 module.exports = function buttonPlugin(md) {
   md.use(mdRegexp(
-      /(^|\s)(\^[^\s]+\^)/i,
+      /(^|\s)(?:\^)([^\s]+)(?:\^)/,
       function(match, utils) {
         return utils.escape(match[1]) + '<button>' + utils.escape(match[2]) + '</button>';
       }
