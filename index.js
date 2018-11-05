55dda4cb5dbe1dde561d5b191823e657063dafa6
Radovan Balog <radovan.balog@2ring.com>
2018-11-05 15:09:36 +0100

    support spaces in buttons

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mdRegexp = require('markdown-it-regexp');
 
 module.exports = function buttonPlugin(md) {
   md.use(mdRegexp(
-      /(^|\s)(?:\^)([^\s]+)(?:\^)/,
+      /(^|\s)(?:\^)([\w ]+)(?:\^)/,
       function(match, utils) {
         return utils.escape(match[1]) + '<button>' + utils.escape(match[2]) + '</button>';
       }
