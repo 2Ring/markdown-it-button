47ae4f141ccd6d9f293e3de7bb5ce16e449b8dd4
Radovan Balog <radovan.balog@2ring.com>
2018-11-05 16:43:12 +0100

    Added support for classes

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var mdRegexp = require('markdown-it-regexp');
 
 module.exports = function buttonPlugin(md) {
   md.use(mdRegexp(
-      /(^|\s)(?:\^)([\w ]+)(?:\^)/,
+      /(^|\s)(?:\^)([\w -]+)(?:\:([\w -]+))?(?:\^)/,
       function(match, utils) {
+        if (match[3]) {
+          return utils.escape(match[1]) + '<button class="' + utils.escape(match[3]) + '">' + utils.escape(match[2]) + '</button>';
+        }
         return utils.escape(match[1]) + '<button>' + utils.escape(match[2]) + '</button>';
       }
   ));
