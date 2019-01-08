348fe3da07a64c3e1be8d255314b8d179bba251c
Radovan <radovan.balog@2ring.com>
2019-01-08 15:10:59 +0100

    Update index.js
    Change button to span

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ module.exports = function buttonPlugin(md) {
       /(^|\s)(?:\^)([\w -]+)(?:\:([\w -]+))?(?:\^)/,
       function(match, utils) {
         if (match[3]) {
-          return utils.escape(match[1]) + '<button class="' + utils.escape(match[3]) + '">' + utils.escape(match[2]) + '</button>';
+          return utils.escape(match[1]) + '<span class="' + utils.escape(match[3]) + '">' + utils.escape(match[2]) + '</span>';
         }
-        return utils.escape(match[1]) + '<button>' + utils.escape(match[2]) + '</button>';
+        return utils.escape(match[1]) + '<span class="btn btn-primary">' + utils.escape(match[2]) + '</span>';
       }
   ));
 }
