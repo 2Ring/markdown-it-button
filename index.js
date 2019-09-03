d55a5154f38995dbe24d3883622a11e7ca5d57f5
Radovan <radovan.balog@2ring.com>
2019-09-03 14:21:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ var mdRegexp = require('markdown-it-regexp');
 
 module.exports = function spanPlugin(md) {
   md.use(mdRegexp(
-      /(^|\s)(?:\^)([\w -]+)(?:\:([\w -]+))?(?:\^)/,
+      /(^|\s)(?:\^)([^:]+)(?:\:([\w -]+))?(?:\^)/,
       function(match, utils) {
+        const text = md.render(match[2]).replace(/<p>|<\/p>/g, '');
         if (match[3]) {
-          return utils.escape(match[1]) + '<span class="' + utils.escape(match[3]) + '">' + utils.escape(match[2]) + '</span>';
+          return utils.escape(match[1]) + '<span class="' + utils.escape(match[3]) + '">' + text + '</span>';
         }
-        return utils.escape(match[1]) + '<span class="btn btn-primary">' + utils.escape(match[2]) + '</span>';
+        return utils.escape(match[1]) + '<span class="btn btn-primary">' + text + '</span>';
       }
   ));
 }
